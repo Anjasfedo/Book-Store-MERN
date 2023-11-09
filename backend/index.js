903ded6181e032be57a591c5c4528cad8858cfb4
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-11-09 13:40:54 +0700

    Request get, put, and delete

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import mongoose from 'mongoose';
 import {PORT, mongoDBURL} from './config.js'
 import {Book} from './models/bookModel.js'
@@ -14,9 +14,12 @@ app.get('/', (request, response) => {
     return response.status(234).send('Hello World')
 });
 
+
+
 // request untuk menyimpan buku baru
 app.post('/books', async (request, response) => {
     try{
+        // validasi
         if(
             !request.body.title ||
             !request.body.author ||
@@ -41,6 +44,83 @@ app.post('/books', async (request, response) => {
     }
 })
 
+// request menampilkan seluruh books
+app.get('/books', async (request, response) => {
+    try{
+        const books = await Book.find({})
+        
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        })
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
+// request menampilkan book berdasarkan id
+app.get('/books/:id', async (request, response) => {
+    try{
+
+        const {id} = request.params;
+
+        const book = await Book.findById(id)
+        
+        return response.status(200).json(book)
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
+// request untuk mengubah(update) book
+app.put('/books/:id', async(request, response) => {
+    try{
+        // validasi
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({message: 'Send all required field: title, author, publishYear'})
+        }
+
+        const {id} = request.params;
+
+        const bookUpdate = await Book.findByIdAndUpdate(id, request.body)
+
+        if(!bookUpdate){
+            response.status(404).json({message: 'Book not found!'})
+        }
+
+        return response.status(200).json({message: 'Book successfuly updated!'})
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
+// request untuk menghapus(delete) book
+app.delete('/books/:id', async (request, response) => {
+    try{
+
+        const {id} = request.params;
+
+        const bookDelete = await Book.findByIdAndDelete(id, request.body)
+
+        if(!bookDelete){
+            response.status(404).json({message: 'Book not found!'})
+        }
+
+        return response.status(200).json({message: 'Book successfuly deleted!'})
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
 
 // 
 
